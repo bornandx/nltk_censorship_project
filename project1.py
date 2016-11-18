d2af284e74db340bbf3c81399154cda7faaa68a9
Brandon <bdude1887@gmail.com>
2016-11-18 10:47:39 -0500

    Actually started with some of the code
    Started to implement a way to fetch training data.
    Added a file to store urls to "good" websites for training.
    Added a file to store urls to "bad" websites for training.

--- project1.py
+++ project1.py
@@ -1,8 +1,44 @@
-<<<<<<< HEAD
 #!/usr/bin/env python3
+
+import nltk
+
+import os
+os.chdir(os.getcwd())
 
 from sklearn.svm import LinearSVC
 from nltk.classify.scikitlearn import SklearnClassifier
 
-def censMain():
-    print("project here")
+def getPage(string):
+    """
+    Takes a weburl as a string and gets the html off the internet.  If the
+    page can't be retrieved for some reason then raise an IndexError.
+    (because internet[thatpage] doesn't exist or couldn't be reached <.< )
+    """
+    html = ""
+    #TODO
+    return html
+
+def getTrainingData(afile):
+    """
+    Takes a file path/name and creates the training blocks of text from the
+    weburls listed in the file.  Returns a list of html pages as strings.
+
+    (Should we parse the html code or not?  It could be relevant to determining
+    if the page should be censored?)
+    """
+    data = []
+    with open(afile, 'r') as trainFile:
+        for line in trainFile:
+            line = line.rstrip() #strip newlines
+            if not line:
+                continue
+            data.append(getPage(line))
+    return data
+
+def censMain(BadTrainingFile, GoodTrainingFile):
+    """
+    Top level logic for running the program.
+    """
+    goodData = getTrainingData(GoodTrainingFile)
+    badData = getTrainingData(BadTrainingFile)
+    #TODO
