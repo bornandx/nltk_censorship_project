c5f51060b5c3ae25bba159ae5c1d942958eaec70
Brandon <bdude1887@gmail.com>
2016-11-18 09:44:25 -0500

    current

--- project1.py
+++ project1.py
@@ -6,19 +6,3 @@ from nltk.classify.scikitlearn import SklearnClassifier
 
 def censMain():
     print("project here")
-=======
-from nltk
-
-
-def censMain():
-    print("project here")
-
-
-
-
-
-
->>>>>>> 988a1bca07f953c29eee5a5ade2c257e47a4c746
-
-if __name__ == "__main__":
-    censMain()
