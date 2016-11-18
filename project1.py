b563a8610507f72ce5ac807f50f2ce55d91a4415
Brandon <bdude1887@gmail.com>
2016-11-18 09:36:05 -0500

    ...

--- project1.py
+++ project1.py
@@ -1,13 +1,10 @@
+#!/usr/bin/env python3
 
-
+from sklearn.svm import LinearSVC
+from nltk.classify.scikitlearn import SklearnClassifier
 
 def censMain():
-print("project here")
+    print("project here")
 
-
-
-
-
-
-
-censMain()
+if __name__ == "__main__":
+    censMain()
