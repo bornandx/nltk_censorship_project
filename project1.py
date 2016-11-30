9507648d18387f2d6fde7c26fd53999143b85d3a
Brandon <bdude1887@gmail.com>
2016-11-30 07:39:10 -0500

    Worked on training data collection
    Now (hopefully) creates a proper training dataset.
    Added a variable to choose whether to recreate/update the
    training data.
    Program now fakes being a browser in the header for url requests
    (This was causing issues because a bunch of sites respond with
    an error otherwise.  Interestingly it was mostly the sites in
    the trainingBad).
    Training html stored as .txt in a specific folder structure
    to utilize sklearn.datasets.load_files for making the dataset.

--- project1.py
+++ project1.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
 import nltk
-import urllib
+import urllib.request
+import re
 
 import os
 os.chdir(os.getcwd())
 
 from sklearn.svm import LinearSVC
+import sklearn
 from nltk.classify.scikitlearn import SklearnClassifier
+
+#TODO make this a command line arg
+MAKETRAINING = False
 
 def getPage(string):
     """
@@ -15,38 +20,59 @@ def getPage(string):
     page can't be retrieved for some reason then raise an IndexError.
     (because internet[thatpage] doesn't exist or couldn't be reached <.< )
     """
+
+    #Lots of sites block non browser user agents so let's fake being a browser.
+    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+    headers = {'User-Agent':user_agent,}
+
     try:
-        html = urllib.request.urlopen(string).read()
+        request = urllib.request.Request(string, None, headers)
+        html = urllib.request.urlopen(request).read()
     except:
         raise IndexError
-    return html
+    return str(html)
 
 def getTrainingData(afile):
     """
     Takes a file path/name and creates the training blocks of text from the
-    weburls listed in the file.  Returns a list of html pages as strings.
+    weburls listed in the file.  Stores the html in .txt files in proper
+    subfolders to later be turned into datasets with
+    sklearn.datasets.load_files.
 
     (Should we parse the html code or not?  It could be relevant to determining
     if the page should be censored?)
     """
-    data = []
-    with open(afile, 'r') as trainFile:
-        for line in trainFile:
-            line = line.rstrip() #strip newlines
-            if not line:
-                continue
-            try:
-                data.append(getPage(line))
-            except:
-                continue
-    return data
+
+    #Make the .txt to be turned into datasets
+    if(MAKETRAINING):
+        with open("training/"+afile, 'r') as trainFile:
+            for line in trainFile:
+                line = line.rstrip() #strip newlines
+                lineNoSlash = re.sub(r'http://','',line)
+                lineNoSlash = re.sub(r'https://','',line)
+                lineNoSlash = re.sub(r'www.','',lineNoSlash)
+                lineNoSlash = re.sub(r'[/<>:"\|?*]','', lineNoSlash)
+                print(lineNoSlash)
+                if not line:
+                    continue
+                try:
+                    with open("data/"+afile[:-4]+"/"+lineNoSlash+".txt", 'w') as htmlFile:
+                        htmlFile.write(getPage(line))
+                except:
+                    print("Error for "+line)
+                    continue
 
 def censMain(BadTrainingFile, GoodTrainingFile):
     """
     Top level logic for running the program.
     """
-    goodData = getTrainingData(GoodTrainingFile)
-    badData = getTrainingData(BadTrainingFile)
-    print(goodData)
+    getTrainingData(GoodTrainingFile)
+    getTrainingData(BadTrainingFile)
+    
+    dataset = sklearn.datasets.load_files("data/")
+    for something in dataset:
+        print(something, type(dataset[something]))
+    for name in dataset["target_names"]:
+        print("NAME:",name)
 
-censMain("training/trainingBad.txt", "training/trainingGood.txt")
+censMain("trainingBad.txt", "trainingGood.txt")
