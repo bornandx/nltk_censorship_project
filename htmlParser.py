ae334979f76c0f6d9f2aeb6454f97dbdc5749843
Quinn Jones <quinnjns3@gmail.com>
2016-12-07 09:58:57 -0500

    Parser now works need to use text[0: ] to print entire item. Returns as an nltk text object not a list or array, will change in future if needed.

--- htmlParser.py
+++ htmlParser.py
@@ -1,12 +1,12 @@
 from bs4 import BeautifulSoup
-
+from nltk import Text
+from nltk import word_tokenize
 
 
 def htmlParser(html):
-    htmlText = BeautifulSoup(html,'html.parser')
-    cleanHtmlText = htmlText.prettify()
-    htmlText = cleanHtmlText.get_text()
-    return htmlText
+    htmlText = BeautifulSoup(html,"html.parser").get_text()
+    text = nltk.Text(word_tokenize(htmlText))
+    return text
 
 
     
